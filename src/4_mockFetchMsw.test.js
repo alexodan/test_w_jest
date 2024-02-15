e9b112a82e01bf137be06adc5bcab88a6e697cfe
Alexis Diaz <alexdan1993@gmail.com>
2024-02-15 17:35:58 -0300

    asserting url with msw events

--- src/4_mockFetchMsw.test.js
+++ src/4_mockFetchMsw.test.js
@@ -3,14 +3,25 @@ import { server, rest } from './testServer';
 import { fetchJoke } from './4_mockFetch';
 
 describe('4_mockFetch - simple', () => {
+  const urls = [];
+
+  beforeAll(() => {
+    server.events.on('request:match', req => {
+      urls.push(req.url.href);
+    });
+  });
+
   it('returns a single-type joke from Programming category without specific keywords', async () => {
     expect(await fetchJoke()).toBe(
       "Knock knock. Who's there? Recursion. Recursion who? Knock knock."
     );
+    expect(urls[0]).toBe('https://v2.jokeapi.dev/joke/Programming');
   });
 
   it('returns a single-type joke from Programming category containing the keyword "bug"', async () => {
-    expect(await fetchJoke('bug')).toBe("bug. that's it that's the joke");
+    const keyword = 'bug';
+    expect(await fetchJoke(keyword)).toBe(`${keyword}. that's it that's the joke`);
+    expect(urls[1]).toBe(`https://v2.jokeapi.dev/joke/Programming?contains=${keyword}`);
   });
 
   it('returns a two-part joke from Programming category with setup and delivery', async () => {
@@ -26,6 +37,7 @@ describe('4_mockFetch - simple', () => {
       })
     );
     expect(await fetchJoke()).toContain('...');
+    expect(urls[2]).toBe('https://v2.jokeapi.dev/joke/Programming');
   });
 
   it('gracefully handles error response structures from the API', async () => {
@@ -41,5 +53,6 @@ describe('4_mockFetch - simple', () => {
       })
     );
     expect(await fetchJoke()).toBe('Something went wrong. Ran out of good jokes');
+    expect(urls[3]).toBe('https://v2.jokeapi.dev/joke/Programming');
   });
 });
